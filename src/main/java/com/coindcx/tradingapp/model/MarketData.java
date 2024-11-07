f31305ac8186d892e47b26ceb6704f2902b6827a
vivek <vivekneymar2002@gmail.com>
2024-11-07 23:27:20 +0530

    .

--- src/main/java/com/coindcx/tradingapp/model/MarketData.java
+++ src/main/java/com/coindcx/tradingapp/model/MarketData.java
@@ -1,12 +1,11 @@
 package com.coindcx.tradingapp.model;
 
-import lombok.Data;
-
-@Data
 public class MarketData {
     private String currencyPair;
     private double lastPrice;
     private double highPrice;
     private double lowPrice;
     private double volume;
+
+    // Getters and setters omitted for brevity
 }
--- src/main/java/com/coindcx/tradingapp/ui/TradingUI.java
+++ src/main/java/com/coindcx/tradingapp/ui/TradingUI.java
@@ -1,7 +1,5 @@
 package com.coindcx.tradingapp.ui;
 
-import com.coindcx.tradingapp.TradingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
@@ -9,16 +7,12 @@ import java.util.Scanner;
 @Component
 public class TradingUI {
 
-    @Autowired
-    private TradingService tradingService;
-
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter trigger price: ");
+        System.out.println("Enter trigger price: ");
         double triggerPrice = scanner.nextDouble();
-        tradingService.setTriggerPrice(triggerPrice);
 
-        System.out.println("Listening for real-time market data...");
+        // Other user inputs or actions can be processed here
     }
 }
